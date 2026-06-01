ac42e1bb2a46258ed57e1a7deb0fe3d0b6fa77eb
Bagus Giovani <bagusgiovani@gmail.com>
2026-06-01 21:22:37 +0700

    feat(pwa): register service worker via SerwistProvider

--- tcs-saas-app/app/client-shell.tsx
+++ tcs-saas-app/app/client-shell.tsx
@@ -1,18 +1,21 @@
-"use client";
+'use client'
 
-import * as React from "react";
-import SplashScreen from "@/components/splash/SplashScreen";
+import * as React from 'react'
+import { SerwistProvider } from '@serwist/next/react'
+import SplashScreen from '@/components/splash/SplashScreen'
 
 export default function ClientShell({ children }: { children: React.ReactNode }) {
   return (
-    <SplashScreen
-      logoSrc="/tcs-logo.png"
-      introMs={2200}
-      collapseMs={1000}
-      zoomMs={900}
-      oncePerSession
-    >
-      {children}
-    </SplashScreen>
-  );
+    <SerwistProvider swUrl="/sw.js">
+      <SplashScreen
+        logoSrc="/tcs-logo.png"
+        introMs={2200}
+        collapseMs={1000}
+        zoomMs={900}
+        oncePerSession
+      >
+        {children}
+      </SplashScreen>
+    </SerwistProvider>
+  )
 }
